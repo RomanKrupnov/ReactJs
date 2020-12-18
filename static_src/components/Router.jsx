f7d3d6c8dee83d893a92631edd0835cab00b33f3
romanKrupnov <89263797575@rambler.ru>
2020-12-18 11:40:53 +0300

    поправки

--- static_src/components/Router.jsx
+++ static_src/components/Router.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import App from './Layout';
+import Profile from "./profile/Profile";
 export default class Router extends React.Component {
     render() {
         return (
             <Switch>
                 <Route exact={ true } path='/' component={ App } />
-                <Route
-                    exact
-                    path='/chat/:chatId/'
-                    render={ obj => <App
-                        chatId={ Number(obj.match.params.chatId) }
+                <Route exact path='/chat/:chatId/' render={ obj => <App chatId={ Number(obj.match.params.chatId) }
                     />
                     }
                 />
-                <Route
-                    exact
-                    path='/profile/'
-                    render={ obj => <App />
-                    }
+                <Route exact path='/profile/' render={() => <Profile /> }
                 />
                 <Redirect to={'/'}/>
             </Switch>
--- static_src/components/profile/Profile.jsx
+++ static_src/components/profile/Profile.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 export default class Profile extends React.Component {
     render(){
         return(
-            <span style={{fontSize:'25px'}}>Profile</span>
-        )
+            <span style={{fontSize:'25px'}}> Profile page</span>)
     }
 }
